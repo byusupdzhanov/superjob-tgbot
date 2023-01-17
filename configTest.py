aebcbb2937807c7298223b3501a592d77065bb74
Bogdan Yusupdzhanov <97046736+getsucked@users.noreply.github.com>
2023-01-17 12:51:02 +0300

    Add files via upload

---
+++ configTest.py
@@ -0,0 +1,37 @@
+settings = {
+    'TOKEN': '5801766295:AAFV91YxnN5tgqxdy1yWcSpRp17R9fVxeXc',
+    'tokens': '838393830384923',
+    'caption_hello': 'Привет! \n\n'
+                     '<b>Думаешь о том, как правильно начать карьеру,</b>'
+                     '<b>найти стажировку или вакансию мечты, составить идеальное резюме или оценить свою стоимость </b>'
+                     '<b>на  рынке труда</b>? \n\n'
+                     'Ты по адресу! \n\n'
+                     '<b>Подписывайся на канал</b> 👇\n'
+                     '<b>https://t.me/studentsuper</b> \n\n'
+                     'Здесь ты найдешь: \n\n'
+                     '— дайджесты с актуальными стажировками и вакансиями с опытом от 0 до 3 лет;\n'
+                     '— розыгрыши курсов и подарков от SuperJob и наших партнеров;\n'
+                     '— анонсы карьерных мероприятий, на которых ты сможешь прокачать себя и найти работу;\n'
+                     '— полезные материалы по написанию резюме, определению карьерного трека, поиску вакансий и т.д.\n'
+                     '— новости из мира карьеры и работодателей.',
+    'photo_hello': 'https://s348vla.storage.yandex.net/rdisk/03172b2b90d76059081a25acb424fa0522fd0bf5da50567bb6dcf61640f00e77/63b03395/xAT9F320_wuTumXz8U_X7mkZK_wObGBm1cFVDZ3xVmkzZMV9ABImrJCVFCkyXkRCkJmka-TTFPmzuf2TPWWrCg==?uid=797670624&filename=%D0%A1%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5%201.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=797670624&fsize=3203747&hid=fccafc7678fd14b1b4d0d7c779054b29&media_type=image&tknv=v2&etag=83b79fa4e0d74be7ef07922796112038&rtoken=G384x8FwaWjO&force_default=yes&ycrid=na-b5e3bde0a5d0a937d141877458bf4e25-downloader9f&ts=5f11f5f144f40&s=ac982241936a8678405c19cb860094d165ca8a392f2876c5aa2139c48825f9f2&pb=U2FsdGVkX19YnU9-A5QXFLhlbghrhS2folGF4uUhxt3THW-Ee7oCnZTmjgUcB7D7W8kcyl6JxLKJtvv4I1f38NO1D-jr8hh6Rino1MH_Wds',
+    'caption_leave': 'Нам очень важно знать твое мнение! \n\n'
+                     'Удели пару минут и поделись причиной '
+                     'отписки в обратном сообщении - мы пришлем тебе подарок 🎁',
+    'photo_leave': 'https://s189vla.storage.yandex.net/rdisk/c7c7f11a52a055f303dba9bf0f120d3f3615dedb2d1673b676009cf9f0ecd5f6/63b0333d/xAT9F320_wuTumXz8U_X7gte1zZ0SarM2DtGowF4aY9JGgzfX4MktrzPLVoh3iBMuXWu_mtY7y2M3S1kbYCvZg==?uid=797670624&filename=%D0%A1%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5%202.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=797670624&fsize=3266602&hid=c28317d903cb3008e27708f00a611166&media_type=image&tknv=v2&etag=03ed491da47be3630c24260358fab22f&rtoken=yiHxd3fW719J&force_default=yes&ycrid=na-79d30a3eb0fa061c573f4b1bf7f460ff-downloader9f&ts=5f11f59d58940&s=22cf88a0d63e164dbcecdfd0fa8dde43edc1d79bccc8d101ae1a8efba7557669&pb=U2FsdGVkX1_12htP_BL02J15IzkJkh70iq_ePeptTa9wWeYWNBU0sBVXlyXFbdi5MChGrpuHKaJaDsshKuy9ep8oHcU1TcBbXXOoLTuMoYI',
+    'caption_answer':'Спасибо за отзыв!\n\n'
+                     '<b>Твой подарок — разбор твоего резюме от наших карьерных экспертов.</b>'
+                     '<b>Они расскажут, как сделать его привлекательным для работодателей.</b>\n\n'
+                     
+                     'Рекомендации придут тебе по электронной почте — просто создай резюме на SuperJob '
+                     'и направь его на проверку. Для этого перейди <a href = "https://superjob.ru/i/3sIxW">по ссылке в'
+                     'форму обратной связи</a> и в теме вопроса укажи <b>промокод «Старт Подарок» </b>💙\n\n'
+                     'Грамотно составленное резюме — залог твоего успеха при поиске работы!',
+    'photo_answer':'https://s126vlx.storage.yandex.net/rdisk/29419f784b5a2c4f147972a952ee7d18170ffd287f20793d64b17aceab8e7efa/63b037f5/xAT9F320_wuTumXz8U_X7szWIf0rDpkQV15Sak3oG5OmLQjZeRiUGAAs7U6ev-UE9IXvHfzspU26aN2_3APNYw==?uid=797670624&filename=%D0%A1%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D0%B5%20%203.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=797670624&fsize=2509572&hid=5a9d76dfd4603833df0045907e1e7dca&media_type=image&tknv=v2&etag=6655bf23c7a9dbda7e8cff12c031f735&rtoken=gGTiBUCYGjPr&force_default=yes&ycrid=na-5d4053e89123c5dcfe93970be8550d4b-downloader23e&ts=5f11fa1d62740&s=452af038e1733091ceab80bd55d4d0898957f145a99e54f9db8801541c835b7b&pb=U2FsdGVkX180O5y7WRsK--L0agPq0zkqkffBc2TLPRw37z30OZ5Rj4thSvCYdynh32bkSf_QR_XlR-m01PWn-ol_ZvqzGNG5niNQsuGshn8',
+}
+
+# For tests
+# https://t.me/studentsuper
+# https://t.me/+e7cXDs8cJd0xZThi
+
+"https://i.ibb.co/kDJKYjZ/1.png"
---
+++ mainTest.py
@@ -0,0 +1,38 @@
+from configTest import settings
+
+import telebot
+
+from telebot import types
+
+bot = telebot.TeleBot(token=settings['TOKEN'], parse_mode='html')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_photo(message.chat.id,
+                   photo=settings['photo_hello'],
+                   caption=settings['caption_hello'.format(message.from_user)],
+                   )
+
+# https://t.me/studentsuperhttps://t.me/+e7cXDs8cJd0xZThi
+
+@bot.chat_member_handler()
+def chat_member_handler(message: telebot.types.Message):
+    status = message.new_chat_member.status
+
+    if status == 'left':
+        bot.send_photo(photo=settings['photo_leave'],
+                       chat_id=message.new_chat_member.user.id,
+                       caption=settings['caption_leave'])
+
+@bot.message_handler(content_types=['text'])
+def send_message(message):
+    markup = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton("Вернуться на канал!", url='https://t.me/studentsuper')
+    markup.add(button1)
+    bot.send_photo(message.chat.id,
+                   photo=settings['photo_answer'],
+                   caption=settings['caption_answer'],
+                   reply_markup=markup
+                   )
+
+bot.infinity_polling(allowed_updates=telebot.util.update_types)
